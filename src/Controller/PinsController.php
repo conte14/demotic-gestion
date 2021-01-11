8d75e3f22d7cbda8a011f013719da63d0b43e16f
conte14 <mohamedconte251@gmail.com>
2021-01-11 12:23:31 +0100

    Modifications

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\Pin;
-use Doctrine\ORM\EntityManagerInterface;
 
-use Repository\PinRepository;
+use App\Entity\Pin;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,38 +22,80 @@ use Symfony\Component\Routing\Annotation\Route;
 class PinsController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_home")
      */
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(PinRepository $repo): Response
     {
-
-     if ($request->isMethod('POST')) {
-         $data = $request->request->all();
-         $Pin = new Pin;
-
-         $Pin->setNom($data['nom']);
-         $Pin->setPrenom($data['prenom']);
-         $Pin->setTelephone($data['telephone']);
-         $Pin->setMail($data['mail']);
-
-          $Pin->setMarque($data['marque']);
-         $Pin->setModele($data['modele']);
-
-         $em->persist($Pin);
-         $em->flush();
-
-     }
-
-        return $this->render('Pins/home.html.twig');
+        return $this->render('Pins/index.html.twig',['Pins'=>$repo->findAll()]);
     }
+
+   
 
      /**
-     * @Route("/pins", name="pins")
+    * @Route("/pins/show", name="app_pins_show")
      */
-
-    public function home()
+    public function show(): Response
     {
-        return $this->render('Pins/index.html.twig');
+        return $this->render('Pins/show.html.twig');
     }
-}
 
+
+
+
+
+
+
+
+
+
+      /**
+     * @Route("/pins", name="app_create")
+     */
+    public function create(Request $request,EntityManagerInterface $em ): Response
+    {
+       $form = $this->createFormBuilder()
+        ->add('nom', TextType::class)
+        ->add('prenom', TextType::class)
+        ->add('telephone',  NumberType::class)
+        ->add('mail', EmailType::class)
+        ->add('marque', TextType::class)
+        ->add('modele', TextType::class)
+        ->add('valider', SubmitType::class)
+         ->getForm()
+    ;
+
+
+
+       //gerer le formulaire pour eviter les petit probleme
+       $form->handleRequest($request);
+        if ($form->isSubmitted () && $form->isValid()) {
+
+
+            $data = $form->getData();
+
+            $Pin = new Pin;
+            $Pin->setPrenom ($data['prenom']);
+            $Pin->setNom($data['nom']);
+            $Pin->setTelephone ($data['telephone']);
+            $Pin->setMail ($data['mail']);
+            $Pin->setMarque($data['marque']);
+            $Pin->setModele($data['modele']);
+
+            $em->persist($Pin);
+            $em->flush();
+
+
+      return $this->redirectToRoute('app_home');
+    
+           
+        }
+
+
+
+        return $this->render('Pins/create.html.twig', [
+            'monFormulaire'=>$form->createView()
+            ]);
+    
+        }
+        
+    }
